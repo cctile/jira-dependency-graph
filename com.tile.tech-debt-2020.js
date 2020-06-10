0078ae108753e9eca88e2154e2d84f42afb86f8c
Charles Choi <charles@thetileapp.com>
2020-06-09 18:48:41 -0700

    Redact now invalidated credentials.

--- com.tile.tech-debt-2020.js
+++ com.tile.tech-debt-2020.js
@@ -10,10 +10,6 @@
         }
     ],
     "EnvironmentVariables" : {
-        "SLACK_API_TOKEN": "xoxp-2978844756-4917347174-1165201882018-04f3845ef21c3e79329fedc3360af268",
-        "JIRA_USER": "charles@tile.com",
-        "JIRA_PASS": "RyaJPcLgwBAS2dkylhYK0B0F",
-        "SLACK_CHANNEL": "#ios-tech-debt-2020,#iosdev"
     },
     "WorkingDirectory": "/Users/cchoi/Projects/jira-dependency-graph",
     "ProgramArguments" : [
